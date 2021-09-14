9b160b211b773f971d6c194628c52de48965de2a
madycissokho <mady6ko@gmail.com>
2021-09-14 09:23:10 +0000

    limits-service1

---
+++ limits-service1/limits-service1/src/main/java/com/saraya/limitsservice1/LimitsService1Application.java
@@ -0,0 +1,13 @@
+package com.saraya.limitsservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsService1Application.class, args);
+	}
+
+}
---
+++ limits-service1/limits-service1/src/main/java/com/saraya/limitsservice1/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.saraya.limitsservice1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saraya.limitsservice1.configuration.Configuration;
+import com.saraya.limitsservice1.controller.bean.Limits;
+
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(configuration.getMaximum(),configuration.getMinimum());//return new Limits(2000, 2);
+	}
+
+}
